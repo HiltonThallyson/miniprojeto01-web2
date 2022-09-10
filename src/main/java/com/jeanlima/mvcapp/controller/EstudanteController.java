ecdfda512ee6468aa5abd7135ce6ab6f78a32490
HiltonThallyson <hilton.thallyson@hotmail.com>
2022-09-10 13:22:15 -0300

    listar por curso e detalhar estudante implementado

---
+++ src/main/java/com/jeanlima/mvcapp/controller/EstudanteController.java
@@ -0,0 +1,73 @@
+package com.jeanlima.mvcapp.controller;
+
+import java.util.List;
+
+import javax.swing.text.html.FormSubmitEvent.MethodType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jeanlima.mvcapp.model.Estudante;
+import com.jeanlima.mvcapp.service.EstudanteService;
+
+@Controller
+@RequestMapping("/estudante")
+public class EstudanteController {
+
+
+    @Autowired
+    @Qualifier("estudanteServiceImpl")
+    EstudanteService estudanteService;
+
+    @RequestMapping("/showForm")
+    public String showFormEstudante(Model model){
+
+        model.addAttribute("estudante", new Estudante());
+        return "estudante/formEstudante";
+    }
+
+    @RequestMapping("/addEstudante")
+    public String showFormEstudante(@ModelAttribute("estudante") Estudante estudante,  Model model){
+
+        estudanteService.salvarEstudante(estudante);
+        model.addAttribute("estudante", estudante);
+        return "estudante/paginaEstudante";
+    }
+
+    @GetMapping("/detalharEstudante")
+    public String detalharEstudante(@RequestParam(value="id") Integer id, Model model) {
+        Estudante estudante = estudanteService.getEstudanteById(id);
+        model.addAttribute("estudante", estudante);
+        return "estudante/paginaEstudante";
+    }
+
+    @RequestMapping("/getListaEstudantes")
+    public String showListaEstudante(Model model){
+
+        List<Estudante> estudantes = estudanteService.getListaEstudante();
+        model.addAttribute("estudantes",estudantes);
+        return "estudante/listaEstudantes";
+
+    }
+
+    @RequestMapping("/getListaEstudantesPorCurso")
+    public String showListaEstudantesPorCurso(Model model) {
+        List<Estudante> estudantesBTI = estudanteService.getListaEstudanteByCurso("BTI");
+        List<Estudante> estudantesEngComp = estudanteService.getListaEstudanteByCurso("EngComp");
+        List<Estudante> estudantesEngSoft = estudanteService.getListaEstudanteByCurso("EngSoft");
+        model.addAttribute("estudantesBTI", estudantesBTI);
+        model.addAttribute("estudantesEngComp", estudantesEngComp);
+        model.addAttribute("estudantesEngSoft", estudantesEngSoft);
+        return "estudante/listaEstudantesPorCurso";
+    }
+
+    
+    
+}
---
+++ src/main/java/com/jeanlima/mvcapp/service/EstudanteServiceImpl.java
@@ -0,0 +1,64 @@
+package com.jeanlima.mvcapp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.jeanlima.mvcapp.model.Estudante;
+
+@Component
+public class EstudanteServiceImpl implements EstudanteService{
+
+    public List<Estudante> estudantes = new ArrayList<Estudante>();    
+
+    @Override
+    public void salvarEstudante(Estudante estudante) {
+        Integer id = estudantes.size()+1;
+        Estudante novoEstudante = new Estudante(estudante.getPrimeiroNome(), estudante.getUltimoNome(), estudante.getCurso(), estudante.getLinguagem(), estudante.getEmail());
+        novoEstudante.setId(id);
+        try{
+            this.estudantes.add(novoEstudante);
+        } catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+        
+        
+    }
+
+    @Override
+    public void deletarEstudante(Estudante estudante) {
+        
+        this.estudantes.remove(estudante);
+        
+    }
+
+    @Override
+    public Estudante getEstudanteById(Integer id) {
+        for (Estudante estudante : estudantes) {
+            if(estudante.getId() == id){
+                return estudante;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Estudante> getListaEstudante() {
+        return this.estudantes;
+    }
+
+    @Override
+    public List<Estudante> getListaEstudanteByCurso(String curso) {
+        List<Estudante> estudantes = new ArrayList<Estudante>();
+        for (Estudante estudante : this.estudantes) {
+            if(estudante.getCurso().equals(curso)){
+                estudantes.add(estudante);
+            }
+        }
+        return estudantes;
+    }
+
+    
+}
